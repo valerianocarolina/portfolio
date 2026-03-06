9c17f2af2adaf7d0f6c1c8907e5c9fd1d771e090
Carolina Valeriano <valeriano.carolina@icloud.com>
2026-03-06 15:42:55 -0300

    feat: add Footer component with social links and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from 'react-router-dom'
 import Header from './components/Header/Header'
 import { AppRoutes } from './routes'
+import { Footer } from './components/Footer/Footer'
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
       <AppRoutes />
+      <Footer />
     </BrowserRouter>
   )
 }
---
+++ src/constants/socialLinks.ts
@@ -0,0 +1,4 @@
+export const socialLinks = {
+  github: 'https://github.com/valerianocarolina',
+  linkedin: 'https://www.linkedin.com/in/carolina-valeriano-9a8122230',
+}
